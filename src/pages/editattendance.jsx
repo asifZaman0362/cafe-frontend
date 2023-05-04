98714e8ce2ed595dc397fa9379ffcfc76d4ea305
Asif Zaman <zero362001@gmail.com>
2023-05-04 16:22:19 +0530

    fixed type issues, updated attendance

--- src/pages/editattendance.jsx
+++ src/pages/editattendance.jsx
@@ -1,9 +1,44 @@
-export default function AttendanceTable(props) {
-  if (!props.entries) return <div>No Records!</div>;
+import { useEffect, useReducer } from "react";
+import { useParams } from "react-router-dom";
+import { get } from "../request";
+
+function attendanceReducer(attendance, action) {
+  switch (action.type) {
+    case "init": {
+      return action.attendance;
+    }
+    case "toggle": {
+      return {
+        ...attendance,
+        entries: attendance.entries.map((entry) => {
+          if (entry.id == action.id) {
+            return { ...entry, present: !entry.present };
+          } else return entry;
+        }),
+      };
+    }
+  }
+}
+
+export default function AttendanceTable() {
+  const { recordId } = useParams("id");
+  const [attendance, dispatch] = useReducer(attendanceReducer, []);
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        let res = await get(`/attendance/getRecord?id=${recordId}`);
+        dispatch({ type: "init", attendance: res.data });
+      } catch (error) {
+        console.error(error);
+      }
+      fetch();
+    };
+  }, [recordId]);
+
   return (
     <div className="attendance_table">
       <h2>Attendance</h2>
-      <h3>Date: {props.date}</h3>
+      <h3>Date: {attendance.date}</h3>
       <table>
         <thead>
           <tr>
@@ -12,11 +47,8 @@ export default function AttendanceTable(props) {
           </tr>
         </thead>
         <tbody>
-          {props.entries.map((entry) => {
+          {attendance.entries.map((entry) => {
             let options = {};
-            if (!props.edit) {
-              options.disabled = true;
-            }
             if (entry.value) {
               options.defaultChecked = true;
             }
@@ -24,14 +56,19 @@ export default function AttendanceTable(props) {
               <tr>
                 <td>{entry.name}</td>
                 <td>
-                  <input type="checkbox" id={entry.id} {...options} />
+                  <input
+                    type="checkbox"
+                    id={entry.id}
+                    {...options}
+                    onChange={() => dispatch({ type: "toggle", id: entry.id })}
+                  />
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>{" "}
-      {getButton(props.edit)}
+      {getButton(true)}
     </div>
   );
 }
